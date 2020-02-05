c3cafd58ab1403adbf963aba5f60778c2a718aa0
genadyms <genadyms@gmail.com>
2020-02-05 23:32:25 +0300

    Added stub data

--- src/main/java/com/gmail/genadyms/timesheet/controller/GroupController.java
+++ src/main/java/com/gmail/genadyms/timesheet/controller/GroupController.java
@@ -4,11 +4,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RestController
 @RequestMapping("group")
 public class GroupController {
+    private static List<Map<String, String>> groups = new ArrayList<Map<String, String>>() {{
+        add(new HashMap<String, String>() {{ put("id", "1"); put("name", "work"); }});
+        add(new HashMap<String, String>() {{ put("id", "2"); put("name", "study"); }});
+        add(new HashMap<String, String>() {{ put("id", "3"); put("name", "sport"); }});
+        add(new HashMap<String, String>() {{ put("id", "4"); put("name", "family"); }});
+        add(new HashMap<String, String>() {{ put("id", "5"); put("name", "friend"); }});
+    }};
+
     @GetMapping
-    public String index() {
-        return "Hello World!";
+    public List<Map<String, String>> index() {
+        return groups;
     }
 }
