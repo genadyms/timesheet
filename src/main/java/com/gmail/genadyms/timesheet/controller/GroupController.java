897fb3894f92b2cbc5bda77b7abbd5504ba00989
genadyms <genadyms@gmail.com>
2020-02-05 23:39:33 +0300

    Add getGroup

--- src/main/java/com/gmail/genadyms/timesheet/controller/GroupController.java
+++ src/main/java/com/gmail/genadyms/timesheet/controller/GroupController.java
@@ -1,6 +1,7 @@
 package com.gmail.genadyms.timesheet.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,15 +14,35 @@ import java.util.Map;
 @RequestMapping("group")
 public class GroupController {
     private static List<Map<String, String>> groups = new ArrayList<Map<String, String>>() {{
-        add(new HashMap<String, String>() {{ put("id", "1"); put("name", "work"); }});
-        add(new HashMap<String, String>() {{ put("id", "2"); put("name", "study"); }});
-        add(new HashMap<String, String>() {{ put("id", "3"); put("name", "sport"); }});
-        add(new HashMap<String, String>() {{ put("id", "4"); put("name", "family"); }});
-        add(new HashMap<String, String>() {{ put("id", "5"); put("name", "friend"); }});
+        add(new HashMap<String, String>() {{
+            put("id", "1");
+            put("name", "work");
+        }});
+        add(new HashMap<String, String>() {{
+            put("id", "2");
+            put("name", "study");
+        }});
+        add(new HashMap<String, String>() {{
+            put("id", "3");
+            put("name", "sport");
+        }});
+        add(new HashMap<String, String>() {{
+            put("id", "4");
+            put("name", "family");
+        }});
+        add(new HashMap<String, String>() {{
+            put("id", "5");
+            put("name", "friend");
+        }});
     }};
 
     @GetMapping
     public List<Map<String, String>> index() {
         return groups;
+    }
+
+    @GetMapping("{id}")
+    public Map<String, String> getGroup(@PathVariable String id) {
+        return groups.stream().filter(group -> group.get("id").equals(id)).findFirst().get();
     }
 }
