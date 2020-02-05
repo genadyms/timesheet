976943d28c687a3ef1472715a982d67e72b131db
genadyms <genadyms@gmail.com>
2020-02-05 23:46:47 +0300

    Add exception handler

--- src/main/java/com/gmail/genadyms/timesheet/controller/GroupController.java
+++ src/main/java/com/gmail/genadyms/timesheet/controller/GroupController.java
@@ -1,5 +1,6 @@
 package com.gmail.genadyms.timesheet.controller;
 
+import com.gmail.genadyms.timesheet.exceptions.NotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,6 +44,7 @@ public class GroupController {
 
     @GetMapping("{id}")
     public Map<String, String> getGroup(@PathVariable String id) {
-        return groups.stream().filter(group -> group.get("id").equals(id)).findFirst().get();
+        return groups.stream().
+                filter(group -> group.get("id").equals(id)).findFirst().orElseThrow(NotFoundException::new);
     }
 }
