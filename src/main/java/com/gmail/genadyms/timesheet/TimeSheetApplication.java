1e604cb04d673011742f6a45759402a5adad4782
= <gmazurkevich@hrsinternational.com>
2020-12-02 15:05:15 +0300

    fixed data source error

--- src/main/java/com/gmail/genadyms/timesheet/TimeSheetApplication.java
+++ src/main/java/com/gmail/genadyms/timesheet/TimeSheetApplication.java
@@ -2,8 +2,9 @@ package com.gmail.genadyms.timesheet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class TimeSheetApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/gmail/genadyms/timesheet/TimesheetApplicationTests.java
+++ src/test/java/com/gmail/genadyms/timesheet/TimesheetApplicationTests.java
@@ -3,10 +3,10 @@ package com.gmail.genadyms.timesheet;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class TimesheetApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
